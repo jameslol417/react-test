6e4ad71e8c2a8add1972ea35d34669fd245c78fe
James Choi <jameslol4177@gmail.com>
2024-08-12 23:53:54 +0900

    reworking from scratch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,53 +1,46 @@
-import { useQueryClient } from "@tanstack/react-query"
-import { useState } from "react"
-import { getPost } from "./api/posts"
-import { CreatePost } from "./CreatePost"
-import Post from "./Post"
-import { PostListInfinite } from "./PostListInfinite"
-import { PostListPaginated } from "./PostListPaginated"
-import PostsList1 from "./PostsList1"
-import PostsList2 from "./PostsList2"
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query"
+
+const POSTS = [
+  { id: 1, title: "Post 1"},
+  { id: 2, title: "Post 2"}
+]
 
 export default function App() {
-  const [currentPage, setCurrentPage] = useState(<PostsList1 />)
   const queryClient = useQueryClient()
+  const postsQuery = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => wait(1000).then(() => [...POSTS])
+  })
 
-  function onHoverPostOneLink() {
-    queryClient.prefetchQuery({
-      queryKey: ["posts", 1],
-      queryFn: () => getPost(1),
-    })
+  const newPostMutation = useMutation({
+    mutationFn: title => {
+      return wait(1000).then(() => {
+        POSTS.push({id: crypto.randomUUID(), title})
+      })
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["posts"])
+    }
+  })
+
+  if(postsQuery.isLoading) return <h1>Loading...</h1>
+  if(postsQuery.isError) {
+    return <pre>{JSON.stringify(postsQuery.error)}</pre>
   }
-
   return (
     <div>
-      <button onClick={() => setCurrentPage(<PostsList1 />)}>
-        Posts List 1
+      {postsQuery.data.map(post => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+      <button 
+      disabled={newPostMutation.isLoading}
+      onClick={() => newPostMutation.mutate("New Post")}>
+        Add New
       </button>
-      <button onClick={() => setCurrentPage(<PostsList2 />)}>
-        Posts List 2
-      </button>
-      <button
-        onMouseEnter={onHoverPostOneLink}
-        onClick={() => setCurrentPage(<Post id={1} />)}
-      >
-        First Post
-      </button>
-      <button
-        onClick={() =>
-          setCurrentPage(<CreatePost setCurrentPage={setCurrentPage} />)
-        }
-      >
-        New Post
-      </button>
-      <button onClick={() => setCurrentPage(<PostListPaginated />)}>
-        Post List Paginated
-      </button>
-      <button onClick={() => setCurrentPage(<PostListInfinite />)}>
-        Post List Infinite
-      </button>
-      <br />
-      {currentPage}
     </div>
   )
+}
+
+function wait(duration) {
+  return new Promise(resolve => setTimeout(resolve, duration))
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,9 +4,7 @@ import App from "./App"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
-const queryClient = new QueryClient({
-  defaultOptions: { queries: { staleTime: 1000 * 60 * 5 } },
-})
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
